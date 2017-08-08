83c57dd9e32385c9950dc3c5fd91db201bf914c9
Tiago Bember Simeao <tbembersimeao@ufl.edu>
2017-08-08 17:54:15 -0400

    Adding project configuration level page.

--- js/xman-menu.js
+++ js/xman-menu.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    $('#control_center_menu span').last().append($('#xman-menu-item-wrapper').html());
+    var $ccItems = $('#control_center_menu span');
+    if ($ccItems.length !== 0) {
+        $ccItems.last().append($('#xman-menu-item-wrapper').html());
+    }
+    else {
+        var $projMenu = $('#projMenuApplications');
+        if ($projMenu.length !== 0) {
+            $projMenu.parent().siblings('.x-panel-bwrap').find('.menubox .menubox').append('<div class="hang">' + $('#xman-menu-item-wrapper').html() + '</div>');
+        }
+    }
 });
